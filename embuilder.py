eba97ebd891049e69e158a0fb82cb826a083b01c
hamlet4401 <38521027+hamlet4401@users.noreply.github.com>
2021-08-10 17:18:22 +0200

    giblib

--- embuilder.py
+++ embuilder.py
@@ -1,110 +1,271 @@
-# Copyright 2016 The Emscripten Authors.  All rights reserved.
+#!/usr/bin/env python3
+# Copyright 2014 The Emscripten Authors.  All rights reserved.
 # Emscripten is available under two separate licenses, the MIT license and the
 # University of Illinois/NCSA Open Source License.  Both these licenses can be
 # found in the LICENSE file.
 
-import os
-import shutil
+"""Tool to manage building of system libraries and ports.
+
+In general emcc will build them automatically on demand, so you do not
+strictly need to use this tool, but it gives you more control over the
+process (in particular, if emcc does this automatically, and you are
+running multiple build commands in parallel, confusion can occur).
+"""
+
+import argparse
 import logging
+import sys
 
-TAG = 'release-2.0.2'
-HASH = 'b9d03061d177f20f4e03f3e3553afd7bfe0c05da7b9a774312b389318e747cf9724e0475e9afff6a64ce31bab0217e2afb2619d75556753fbbb6ecafa9775219'
-
-
-def needed(settings):
-  return settings.USE_SDL_MIXER == 2
+from tools import shared
+from tools import system_libs
+import emscripten
 
 
-def get(ports, settings, shared):
-  sdl_build = os.path.join(ports.get_build_dir(), 'sdl2')
-  assert os.path.exists(sdl_build), 'You must use SDL2 to use SDL2_mixer'
-  ports.fetch_project('sdl2_mixer', 'https://github.com/emscripten-ports/SDL2_mixer/archive/' + TAG + '.zip', 'SDL2_mixer-' + TAG, sha512hash=HASH)
+SYSTEM_LIBRARIES = system_libs.Library.get_all_variations()
+SYSTEM_TASKS = list(SYSTEM_LIBRARIES.keys())
 
-  settings.SDL2_MIXER_FORMATS.sort()
-  formats = '-'.join(settings.SDL2_MIXER_FORMATS)
+# This is needed to build the generated_struct_info.json file.
+# It is not a system library, but it needs to be built before running with FROZEN_CACHE.
+SYSTEM_TASKS += ['struct_info']
 
-  libname = 'libSDL2_mixer'
-  if formats != '':
-    libname += '_' + formats
-  libname += '.a'
+# Minimal subset of SYSTEM_TASKS used by CI systems to build enough to useful
+MINIMAL_TASKS = [
+    'libcompiler_rt',
+    'libc',
+    'libc++abi',
+    'libc++abi-except',
+    'libc++abi-noexcept',
+    'libc++',
+    'libc++-except',
+    'libc++-noexcept',
+    'libal',
+    'libdlmalloc',
+    'libdlmalloc-debug',
+    'libemmalloc',
+    'libemmalloc-debug',
+    'libemmalloc-memvalidate',
+    'libemmalloc-verbose',
+    'libemmalloc-memvalidate-verbose',
+    'libgl',
+    'libhtml5',
+    'libsockets',
+    'libc_rt_wasm',
+    'struct_info',
+    'libstandalonewasm',
+    'crt1',
+    'libunwind-except'
+]
 
-  def create(final):
-    logging.info('building port: sdl2_mixer')
+USER_TASKS = [
+    'boost_headers',
+    'bullet',
+    'bzip2',
+    'cocos2d',
+    'freetype',
+    'giflib',
+    'harfbuzz',
+    'icu',
+    'libjpeg',
+    'libpng',
+    'ogg',
+    'regal',
+    'regal-mt',
+    'sdl2',
+    'sdl2-mt',
+    'sdl2-gfx',
+    'sdl2-image',
+    'sdl2-image-png',
+    'sdl2-image-jpg',
+    'sdl2-mixer',
+    'sdl2-mixer-ogg',
+    'sdl2-mixer-mp3',
+    'sdl2-net',
+    'sdl2-ttf',
+    'vorbis',
+    'zlib',
+]
 
-    source_path = os.path.join(ports.get_dir(), 'sdl2_mixer', 'SDL2_mixer-' + TAG)
-    dest_path = os.path.join(ports.get_build_dir(), 'sdl2_mixer')
-
-    shutil.rmtree(dest_path, ignore_errors=True)
-    shutil.copytree(source_path, dest_path)
-
-    flags = [
-      '-s', 'USE_SDL=2',
-      '-O2',
-      '-DMUSIC_WAV',
-    ]
-
-    if "ogg" in settings.SDL2_MIXER_FORMATS:
-      flags += [
-        '-s', 'USE_VORBIS=1',
-        '-DMUSIC_OGG',
-      ]
-
-    if "mp3" in settings.SDL2_MIXER_FORMATS:
-      flags += [
-        '-s', 'USE_MPG123=1',
-        '-DMUSIC_MP3_MPG123',
-      ]
-
-    if "mod" in settings.SDL2_MIXER_FORMATS:
-      flags += [
-        '-s', 'USE_MODPLUG=1',
-        '-DMUSIC_MOD_MODPLUG',
-      ]
-
-    ports.build_port(
-      dest_path,
-      final,
-      includes=[],
-      flags=flags,
-      exclude_files=[
-        'playmus.c',
-        'playwave.c',
-      ],
-      exclude_dirs=[
-        'native_midi',
-        'timidity',
-        'external',
-      ]
-    )
-
-    # copy header to a location so it can be used as 'SDL2/'
-    ports.install_headers(source_path, pattern='SDL_*.h', target='SDL2')
-
-  return [shared.Cache.get_lib(libname, create, what='port')]
-
-
-def clear(ports, settings, shared):
-  shared.Cache.erase_lib('libSDL2_mixer.a')
+temp_files = shared.configuration.get_temp_files()
+logger = logging.getLogger('embuilder')
+force = False
 
 
-def process_dependencies(settings):
-  global deps
-  deps = ['sdl2']
-  settings.USE_SDL = 2
-  if "ogg" in settings.SDL2_MIXER_FORMATS:
-    deps.append('vorbis')
-    settings.USE_VORBIS = 1
-  if "mp3" in settings.SDL2_MIXER_FORMATS:
-    deps.append('mpg123')
-    settings.USE_MPG123 = 1
-  if "mod" in settings.SDL2_MIXER_FORMATS:
-    deps.append('libmodplug')
-    settings.USE_MODPLUG = 1
+def get_help():
+  all_tasks = SYSTEM_TASKS + USER_TASKS
+  all_tasks.sort()
+  return '''
+Available targets:
+
+  build %s
+
+Issuing 'embuilder.py build ALL' causes each task to be built.
+''' % '\n        '.join(all_tasks)
 
 
-def process_args(ports):
-  return []
+def build_port(port_name):
+  if force:
+    system_libs.clear_port(port_name, shared.Settings)
+
+  system_libs.build_port(port_name, shared.Settings)
 
 
-def show():
-  return 'SDL2_mixer (USE_SDL_MIXER=2; zlib license)'
+def main():
+  global force
+  parser = argparse.ArgumentParser(description=__doc__,
+                                   formatter_class=argparse.RawDescriptionHelpFormatter,
+                                   epilog=get_help())
+  parser.add_argument('--lto', action='store_true', help='build bitcode object for LTO')
+  parser.add_argument('--pic', action='store_true',
+                      help='build relocatable objects for suitable for dynamic linking')
+  parser.add_argument('--force', action='store_true',
+                      help='force rebuild of target (by removing it first)')
+  parser.add_argument('operation', help='currently only "build" is supported')
+  parser.add_argument('targets', nargs='+', help='see below')
+  args = parser.parse_args()
+
+  if args.operation != 'build':
+    shared.exit_with_error('unfamiliar operation: ' + args.operation)
+
+  # process flags
+
+  # Check sanity so that if settings file has changed, the cache is cleared here.
+  # Otherwise, the cache will clear in an emcc process, which is invoked while building
+  # a system library into the cache, causing trouble.
+  shared.check_sanity()
+
+  if args.lto:
+    shared.Settings.LTO = "full"
+
+  if args.pic:
+    shared.Settings.RELOCATABLE = 1
+
+  if args.force:
+    force = True
+
+  # process tasks
+  auto_tasks = False
+  tasks = args.targets
+  if 'SYSTEM' in tasks:
+    tasks = SYSTEM_TASKS
+    auto_tasks = True
+  elif 'USER' in tasks:
+    tasks = USER_TASKS
+    auto_tasks = True
+  elif 'MINIMAL' in tasks:
+    tasks = MINIMAL_TASKS
+    auto_tasks = True
+  elif 'ALL' in tasks:
+    tasks = SYSTEM_TASKS + USER_TASKS
+    auto_tasks = True
+  if auto_tasks:
+    # cocos2d: must be ported, errors on
+    # "Cannot recognize the target platform; are you targeting an unsupported platform?"
+    skip_tasks = ['cocos2d']
+    tasks = [x for x in tasks if x not in skip_tasks]
+    print('Building targets: %s' % ' '.join(tasks))
+  for what in tasks:
+    logger.info('building and verifying ' + what)
+    if what in SYSTEM_LIBRARIES:
+      library = SYSTEM_LIBRARIES[what]
+      if force:
+        library.erase()
+      library.get_path()
+    elif what == 'sysroot':
+      if force:
+        shared.Cache.erase_file('sysroot_install.stamp')
+      system_libs.ensure_sysroot()
+    elif what == 'struct_info':
+      if force:
+        shared.Cache.erase_file('generated_struct_info.json')
+      emscripten.generate_struct_info()
+    elif what == 'icu':
+      build_port('icu')
+    elif what == 'zlib':
+      shared.Settings.USE_ZLIB = 1
+      build_port('zlib')
+      shared.Settings.USE_ZLIB = 0
+    elif what == 'bzip2':
+      build_port('bzip2')
+    elif what == 'bullet':
+      build_port('bullet')
+    elif what == 'vorbis':
+      build_port('vorbis')
+    elif what == 'ogg':
+      build_port('ogg')
+    elif what == 'giflib':
+      build_port('giflib')
+    elif what == 'libjpeg':
+      build_port('libjpeg')
+    elif what == 'libpng':
+      build_port('libpng')
+    elif what == 'sdl2':
+      build_port('sdl2')
+    elif what == 'sdl2-mt':
+      shared.Settings.USE_PTHREADS = 1
+      build_port('sdl2')
+      shared.Settings.USE_PTHREADS = 0
+    elif what == 'sdl2-gfx':
+      build_port('sdl2_gfx')
+    elif what == 'sdl2-image':
+      build_port('sdl2_image')
+    elif what == 'sdl2-image-png':
+      shared.Settings.SDL2_IMAGE_FORMATS = ["png"]
+      build_port('sdl2_image')
+      shared.Settings.SDL2_IMAGE_FORMATS = []
+    elif what == 'sdl2-image-jpg':
+      shared.Settings.SDL2_IMAGE_FORMATS = ["jpg"]
+      build_port('sdl2_image')
+      shared.Settings.SDL2_IMAGE_FORMATS = []
+    elif what == 'sdl2-net':
+      build_port('sdl2_net')
+    elif what == 'sdl2-mixer':
+      old_formats = shared.Settings.SDL2_MIXER_FORMATS
+      shared.Settings.SDL2_MIXER_FORMATS = []
+      build_port('sdl2_mixer')
+      shared.Settings.SDL2_MIXER_FORMATS = old_formats
+    elif what == 'sdl2-mixer-ogg':
+      old_formats = shared.Settings.SDL2_MIXER_FORMATS
+      shared.Settings.SDL2_MIXER_FORMATS = ["ogg"]
+      build_port('sdl2_mixer')
+      shared.Settings.SDL2_MIXER_FORMATS = old_formats
+    elif what == 'sdl2-mixer-mp3':
+      old_formats = shared.Settings.SDL2_MIXER_FORMATS
+      shared.Settings.SDL2_MIXER_FORMATS = ["mp3"]
+      build_port('sdl2_mixer')
+      shared.Settings.SDL2_MIXER_FORMATS = old_formats
+    elif what == 'freetype':
+      build_port('freetype')
+    elif what == 'harfbuzz':
+      build_port('harfbuzz')
+    elif what == 'harfbuzz-mt':
+      shared.Settings.USE_PTHREADS = 1
+      build_port('harfbuzz')
+      shared.Settings.USE_PTHREADS = 0
+    elif what == 'sdl2-ttf':
+      build_port('sdl2_ttf')
+    elif what == 'cocos2d':
+      build_port('cocos2d')
+    elif what == 'regal':
+      build_port('regal')
+    elif what == 'regal-mt':
+      shared.Settings.USE_PTHREADS = 1
+      build_port('regal')
+      shared.Settings.USE_PTHREADS = 0
+    elif what == 'boost_headers':
+      build_port('boost_headers')
+    elif what == 'mpg123':
+      build_port('mpg123')
+    else:
+      logger.error('unfamiliar build target: ' + what)
+      return 1
+
+    logger.info('...success')
+  return 0
+
+
+if __name__ == '__main__':
+  try:
+    sys.exit(main())
+  except KeyboardInterrupt:
+    logger.warning("KeyboardInterrupt")
+    sys.exit(1)
